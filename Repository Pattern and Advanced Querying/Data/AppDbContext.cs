50048505018a5355a6b98702fb4ccd0ee9c8ae8f
Ziad Yousef <120646398+ziadyousef1@users.noreply.github.com>
2024-07-29 18:07:12 +0200

    Initial commit

---
+++ Repository Pattern and Advanced Querying/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository_Pattern_and_Advanced_Querying.Models;
+
+namespace Repository_Pattern_and_Advanced_Querying.Data
+{
+    public class AppDbContext : DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Repository Pattern and Advanced Querying/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Repository_Pattern_and_Advanced_Querying.Models;
+
+namespace Repository_Pattern_and_Advanced_Querying.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
